dbcb4d0fa87dc6d0d97992f17ffd1330aa532ee2
Jura <mr.gordien@mail.ru>
2022-01-21 20:51:42 +0300

    add-components
    Add: From, Note, Alert

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 
-export const About = () => {
-    return (
-        <Fragment>
-            <h1>About page</h1>
-        </Fragment>
-    )
-}
+export const About = () => (
+    <div className="jumbotron ">
+        <div className="container">
+            <h1 className="display-3">Лучшее React приложение</h1>
+            <p className="lead">Версия приложения: <strong>1.0</strong></p>
+        </div>
+    </div>
+)
