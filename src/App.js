c103ee194aeabe218fab9a26346051a53eff0c03
Jura <mr.gordien@mail.ru>
2022-01-21 19:20:23 +0300

    routing-and-pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello world !</h1>
-    </div>
+      <BrowserRouter>
+          <div className="container pt-4">
+              <Switch>
+                <Route  path={'/'} exact component={Home}/>
+                <Route  path={'/about'} component={About}/>
+              </Switch>
+          </div>
+      </BrowserRouter>
+
   );
 }
 
