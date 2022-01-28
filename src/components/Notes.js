7059ae3e6065b2ad6e9bed72cd130bec59b4cfa8
Jura <mr.gordien@mail.ru>
2022-01-28 05:16:50 +0300

    add-remove-note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,27 +1,26 @@
 import React from 'react'
 
-export const Notes = ({notes}) => {
-    return (
-        <ul className="list-group">
-            {notes.map(note => (
-                <li
-                    className="list-group-item note"
-                    key={note.id}
+export const Notes = ({notes, onRemove}) => (
+    <ul className="list-group">
+        {notes.map(note => (
+            <li
+                className="list-group-item note"
+                key={note.id}
+            >
+                <div>
+                    <strong>{note.title}</strong>
+                    <small>{note.date}</small>
+                </div>
+
+                <button
+                    type="button"
+                    className="btn btn-outline-danger btn-sm"
+                    onClick={() => onRemove(note.id)}
                 >
-                    <div>
-                        <strong>{note.title}</strong>
-                        <small>{note.date}</small>
-                    </div>
+                    &times;
+                </button>
+            </li>
 
-                    <button
-                        type="button"
-                        className="btn btn-outline-danger btn-sm"
-                    >
-                         &times;
-                    </button>
-                </li>
-
-            ))}
-        </ul>
-    )
-}
+        ))}
+    </ul>
+)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import {FirebaseContext} from "../context/firebase/firebaseContext";
 import {Loader} from "../components/Loader";
 
 export const Home = () => {
-    const {loading,notes, fetchNotes} = useContext(FirebaseContext)
+    const {loading,notes, fetchNotes, removeNote} = useContext(FirebaseContext)
 
     useEffect(() => {
         fetchNotes()
@@ -19,7 +19,7 @@ export const Home = () => {
         <hr/>
         {loading
         ? <Loader />
-        : <Notes notes={notes}/>
+        : <Notes notes={notes} onRemove={removeNote}/>
         }
     </Fragment>
     )
