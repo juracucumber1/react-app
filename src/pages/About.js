8d46fe8a6648cf6c9a98e95d56a9e4fd36ac49a9
Jura <mr.gordien@mail.ru>
2022-01-29 05:58:37 +0300

    'update'

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,11 @@ import React from 'react'
 export const About = () => (
     <div className="jumbotron ">
         <div className="container">
-            <h1 className="display-3">Лучшее React приложение</h1>
-            <p className="lead">Версия приложения: <strong>1.0</strong></p>
+            <h1 className="display-3">Самый легендарный сайт в мире. Его написал Я Жура Кукумбер </h1>
+            <h2 className="display-3">Мой Git: 'https://github.com/juracucumber1'</h2>
+            <h2 className="display-3">Мой Instagram: 'https://www.instagram.com/juracucumber/' </h2>
+            <h3 className="display-3">Желаемая ставка: 150р/час. </h3>
+            <p className="lead">Версия приложения: <strong>2.2.8</strong></p>
         </div>
     </div>
 )
