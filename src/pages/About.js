d55429e5d525f2d6c6a6a62a2645c2d9b022e225
Jura <mr.gordien@mail.ru>
2022-01-29 05:59:56 +0300

    'update'

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,9 @@ export const About = () => (
     <div className="jumbotron ">
         <div className="container">
             <h1 className="display-3">Самый легендарный сайт в мире. Его написал Я Жура Кукумбер </h1>
-            <h2 className="display-3">Мой Git: 'https://github.com/juracucumber1'</h2>
-            <h2 className="display-3">Мой Instagram: 'https://www.instagram.com/juracucumber/' </h2>
-            <h3 className="display-3">Желаемая ставка: 150р/час. </h3>
+            <h2 className="display-4">Мой Git: 'https://github.com/juracucumber1'</h2>
+            <h2 className="display-4">Мой Instagram: 'https://www.instagram.com/juracucumber/' </h2>
+            <h3 className="display-4">Желаемая ставка: 150р/час. </h3>
             <p className="lead">Версия приложения: <strong>2.2.8</strong></p>
         </div>
     </div>
