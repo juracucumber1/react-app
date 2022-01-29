eed8719bb93079675e27c9b041faf1fb50cd56db
Jura <mr.gordien@mail.ru>
2022-01-29 07:10:11 +0300

    'update'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-danger">
+    <nav className="navbar navbar-dark navbar-expand-lg bg-success">
         <div className="navbar-brand">
             Note App
         </div>
